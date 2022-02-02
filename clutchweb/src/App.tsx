c727502ae34e6a403d64381152730f334b93f4dc
kdnelson <kris.d.nelson@gmail.com>
2022-02-01 19:29:49 -0800

    Basic API call with Grid display

--- clutchweb/src/App.tsx
+++ clutchweb/src/App.tsx
@@ -1,7 +1,53 @@
 import React from 'react';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+import { Wrapper } from './App.styles';
+
+import Drawer from '@material-ui/core/Drawer';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+import Item from './Item/Item';
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+// The interior await calls the api and the exterior is converting to JSON
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
-  return <div className="App">Start</div>;
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products', 
+    getProducts
+  );
+
+  const getTotalItems = () => null;
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleRemoveFromCart = () => null;
+
+  if(isLoading) return <CircularProgress />;
+  if(error) return <div>Something went wrong!</div>;
+
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
 }
 
 export default App;
---
+++ clutchweb/src/Item/Item.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { CartItemType } from '../App';
+import { Wrapper } from './Item.style';
+
+type Props = {
+    item: CartItemType;
+    handleAddToCart: (clickedItem: CartItemType) => void;
+}
+
+const Item: React.FC<Props> = ({ item, handleAddToCart}) => (
+    <Wrapper>
+        <img src={ item.image } alt={item.title} />
+        <div>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <h3>${item.price}</h3>
+        </div>
+        {/* Inline arrow function to call and wait for operation to complete
+        You can create the same thing up in the component */}
+        <Button onClick={() => handleAddToCart(item)}>Add To Cart</Button>
+    </Wrapper>
+);
+
+export default Item;
