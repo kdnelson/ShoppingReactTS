00bb4dafaed6547a8da93eab835d92e461454fd7
kdnelson <kris.d.nelson@gmail.com>
2022-02-09 19:35:52 -0800

    Nice working shopping app
    Add a loyalty/discount method calculator
    Cleanup how the cart image looks on the page (push the grid down?)
    Disable the cart button is no items in cart

--- clutchweb/src/App.tsx
+++ clutchweb/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { useQuery } from 'react-query';
-import { Wrapper } from './App.styles';
+import { Wrapper, StyledButton } from './App.styles';
 
 import Drawer from '@material-ui/core/Drawer';
 import CircularProgress from '@material-ui/core/CircularProgress';
@@ -9,6 +9,7 @@ import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 import Item from './Item/Item';
+import Cart from './Cart/Cart';
 
 export type CartItemType = {
   id: number;
@@ -25,20 +26,58 @@ const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
   const { data, isLoading, error } = useQuery<CartItemType[]>(
     'products', 
     getProducts
   );
 
-  const getTotalItems = () => null;
-  const handleAddToCart = (clickedItem: CartItemType) => null;
-  const handleRemoveFromCart = () => null;
+  const getTotalItems = (items: CartItemType[]) => items.reduce((ack: number, item) => ack + item.amount, 0);
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems(prev => {
+      const isItemInCart = prev.find(item => item.id === clickedItem.id);
+      if(isItemInCart) {
+        return prev.map(item =>
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1}
+            : item
+        );
+      } else {
+        return [...prev, { ...clickedItem, amount: 1 }]
+      }
+    })
+  };
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems(prev => (
+      prev.reduce((ack, item) => {
+        if(item.id === id){
+          if(item.amount === 1) return ack;
+          return [...ack, { ...item, amount: item.amount - 1}]
+        } else {
+          return [...ack, item];
+        }
+      }, [] as CartItemType[]) 
+    ))
+  };
 
   if(isLoading) return <CircularProgress />;
   if(error) return <div>Something went wrong!</div>;
 
   return (
     <Wrapper>
+      <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+        <Cart 
+          cartItems={cartItems} 
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+        />
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map(item => (
           <Grid item key={item.id} xs={12} sm={4}>
---
+++ clutchweb/src/Cart/Cart.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import CartItem from '../CartItem/CartItem';
+import { Wrapper } from './Cart.styles';
+import { CartItemType } from '../App';
+import { isTemplateSpan } from 'typescript';
+
+type Props = {
+  cartItems: CartItemType[];
+  addToCart: (clickItem: CartItemType) => void;
+  removeFromCart: (id: number) => void;
+};
+
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart}) => {
+  const calculateTotal = (items: CartItemType[]) =>
+    items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
+
+  return (
+    <Wrapper>
+      <h2>Your Shopping Cart</h2>
+      {cartItems.length === 0 ? <p>Cart is empty.</p> : null}
+      {cartItems.map(item => (
+        <CartItem
+          key={item.id}
+          item={item}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+        />
+      ))}
+      <br/>
+      <br/>
+      <h2 className="cart-total">Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
+    </Wrapper>
+  )
+};
+
+export default Cart;
